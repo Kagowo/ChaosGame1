dc9b3560313c06117bd9e92331c0a6e2523b39d4
Your Name <you@example.com>
2023-11-15 22:10:10 -0800

    commit from Ubuntu

--- ChaosGame/code/ChaosGame.cpp
+++ ChaosGame/code/ChaosGame.cpp
@@ -9,26 +9,47 @@ using namespace std;
 
 int main()
 {
-	VideoMode vm(1920, 1080);
-	RenderWindow window(vm, "Chaos Game", Style::Default);
-
-    vector<Vector2f> vertices;
-    vector<Vector2f> points;
-
-	//font
+        //font
         sf::Font font;
         if (!font.loadFromFile("Arial.ttf"))
         {   
         cerr << "Error" << endl;
         return -1;
         }
-	//setting the text
+	    //setting the text
         Text instructionText;
         instructionText.setFont(font);
         instructionText.setCharacterSize(20);
         instructionText.setFillColor(Color::White);
+        int verts;
+        cout << "Choose amount of vertices 3-5" << endl;
+        cin >> verts;
+        while(verts != 3 && verts != 4 && verts != 5)
+        {
+            cout << "Error: Choose amount of vertices 3-5" << endl;
+            cin >> verts;
+        }
+        if(verts == 3)
+        {
         instructionText.setString("                                                                                                                 Click on any three points to create the vertices for the triangle.");
-   
+        }
+        else if(verts == 4)
+        {
+        instructionText.setString("                                                                                                              Click on any four points to create the vertices for the square/rectangle");
+        }
+        else if(verts == 5)
+        {
+        instructionText.setString("                                                                                                                 Click on any five points to create the vertices for the pentagon");
+        }
+
+
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Chaos Game", Style::Default);
+
+    vector<Vector2f> vertices;
+    vector<Vector2f> points;
+
+
 
 	while (window.isOpen())
 	{
@@ -52,7 +73,7 @@ int main()
                     std::cout << "mouse x: " << event.mouseButton.x << std::endl;
                     std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-                    if(vertices.size() < 3)
+                    if(vertices.size() < verts)
                     {
 			//adds to vertices vector
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
@@ -72,28 +93,72 @@ int main()
 			window.close();
 		}
 
-        if(points.size() > 0)
+        if(verts == 3)
         {
-     	    //current point
-            Vector2f lastPoint = points.back();
-	    //random vertex
-            Vector2f randomVertex = vertices[rand() % vertices.size()];
-	    //calculates midpoint
-            Vector2f midpoint = (lastPoint + randomVertex) / 2.0f;
-	    //pushes new point back into vector
-            points.push_back(midpoint);
+            if(points.size() > 0)
+            {
+                for(int x = 0; x < 50; x++)
+                {
+     	        //current point
+                Vector2f lastPoint = points.back();
 
-            Vector2f lastPoint1 = points.back();
-            Vector2f randomVertex1 = vertices[rand() % vertices.size()];
-            Vector2f midpoint1 = (lastPoint1 + randomVertex1) / 2.0f;
-            points.push_back(midpoint1);
+	            //random vertex
+                Vector2f randomVertex = vertices[rand() % vertices.size()];
 
-            Vector2f lastPoint2 = points.back();
-            Vector2f randomVertex2 = vertices[rand() % vertices.size()];
-            Vector2f midpoint2 = (lastPoint2 + randomVertex2) / 2.0f;
-            points.push_back(midpoint2);
+	            //calculates midpoint
+                Vector2f midpoint = (lastPoint + randomVertex) / 2.0f;
 
+	            //pushes new point back into vector
+                points.push_back(midpoint);
+                }
+            }
+        }
         
+        if(verts == 4)
+        {
+            if(points.size() > 0)
+            {
+                for(int x = 0; x < 50; x++)
+                {
+                    Vector2f lastPoint = points.back();
+                    int randVert;
+                    int prevVert;
+                    randVert = rand() % verts;
+                    while(randVert == prevVert + 2 || randVert == prevVert - 2)
+                    {
+                        randVert = rand() % verts;
+                        Vector2f randomVertex = vertices[randVert];
+                    }
+                    Vector2f randomVertex = vertices[randVert];
+                    Vector2f midpoint = (lastPoint + randomVertex) / 2.0f;
+                    prevVert = randVert;
+                    points.push_back(midpoint);
+                }
+            }
+
+        }
+
+        if(verts == 5)
+        {
+            if(points.size() > 0)
+            {
+                for(int x = 0; x < 50; x++)
+                {
+                    Vector2f lastPoint = points.back();
+                    int randVert;
+                    int prevVert;
+                    randVert = rand() % verts;
+                    while(randVert == prevVert)
+                    {
+                        randVert = rand() % verts;
+                        Vector2f randomVertex = vertices[randVert];
+                    }
+                    Vector2f randomVertex = vertices[randVert];
+                    Vector2f midpoint = (lastPoint + randomVertex) / 2.0f;
+                    prevVert = randVert;
+                    points.push_back(midpoint);
+                }
+            }
         }
         
         window.clear();
